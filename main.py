17e82079a9cf890135262248e5127a769e2951d7
mila <oktiabrina.sinkevich@gmail.com>
2020-08-11 19:28:11 +0200

    Py skeleton

---
+++ main.py
@@ -0,0 +1,34 @@
+import difflib
+from itertools import product
+
+
+def evalWithoutHidden(hidden, turn):
+    solver = difflib.SequenceMatcher(a=hidden, b=turn)
+    solve = solver.find_longest_match(0, len(solver.a), 0, len(solver.b))
+    wholes = solve.b
+    halves = solve.a
+    return wholes, halves
+
+def allCombisWithRep():
+    dig = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    combisWithRep = []
+    for d in product(dig, repeat=4):
+        combisWithRep.append(''.join(d))
+    return combisWithRep
+
+def allCombisWithoutRep():
+    combisWithRep = allCombisWithRep()
+    combisWithoutRep = []
+    for combi in combisWithRep:
+        if len(set(combi)) == len(combi):
+            combisWithoutRep.append(combi)
+    return combisWithoutRep
+
+def evalPossibleWH(turnResult):
+    combis = list()
+    for i in range(0, 4):
+        for j in range(0, 4):
+            if (i + j * 0.5 == turnResult) and (i + j <= 4):
+                combis.append([i, j])
+    return combis
+
